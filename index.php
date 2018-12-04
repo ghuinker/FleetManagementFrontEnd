099c0b8fffed8ebc61b4ca3aebfa0add2653c0e1
vm-user <vm-user@se329-01.ece.iastate.edu>
2018-12-04 16:49:48 -0600

    Moving to Jake Code

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$m = new \MongoDB\Driver\Manager("mongodb://<TeamFunTime>:<TFT2018>@ds147073.mlab.com:47073/fleetdb");
-echo "Database Connected";
-var_dump($m);
-echo "<p>Data</p>";
-$query = new \MongoDB\Driver\Command(['find' => 'vehicles']);
-var_dump($cmd);
-?>
